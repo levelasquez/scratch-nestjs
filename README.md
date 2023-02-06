9b2d898fd31a53003a6ea1e887647c1339c5c9b4
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-02-06 12:36:14 -0300

    Starting Up a Nest App

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # scratch-nestjs
 
 Example nestjs application from scratch wihtout cli
+
+To Run the project:
+
+```sh
+npx ts-node-dev src/main.ts
+```
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { NestFactory } from "@nestjs/core";
 import { Controller, Module, Get } from "@nestjs/common";
 
 @Controller()
@@ -7,3 +8,16 @@ class AppController {
     return "hi there!";
   }
 }
+
+@Module({
+  controllers: [AppController],
+})
+class AppModule {}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(3000);
+}
+
+bootstrap();
